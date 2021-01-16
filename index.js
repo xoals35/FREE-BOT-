f96adef4b4ea46a51ff1b576533f8657740674d2
xoals35 <sinby021110@gmail.com>
2021-01-16 10:25:13 +0900

    botcommit

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const bot = new Client({
 })
 const config = require('./config.json')
 require('./utils/misc.js')
+const mongoose = require("mongoose");
 const prefix = config.prefix;
 const token = config.token;
 bot.commands = new Collection();
 bot.aliases = new Collection();
 bot.events = new Collection();
+bot.snipes = new Collection();
 bot.categories = fs.readdirSync("./commands/");
 const Timeout = new Set();
 ["command","event"].forEach(handler=>{
     require(`./handlers/${handler}`)(bot);
 });
-
-
 
 
 
